163ada7348c9d3f3a41885dfb771f63a688654e9
Steven Ballaret <135417330+SteveBllrt@users.noreply.github.com>
2024-10-15 21:29:18 +0800

    chore: initial project setup

---
+++ src/app/(auth)/login.tsx
@@ -0,0 +1,56 @@
+import React from "react";
+import { Link, Stack } from "expo-router";
+// import LoginForm from "@/components/Form/LoginForm";
+import { View, Text, ScrollView, SafeAreaView, Platform } from "react-native";
+import * as WebBrowser from "expo-web-browser";
+// import GoogleSignInButton from "@/components/Button/GoogleSignInButton";
+
+// Hook to warm up and cool down the web browser for authentication.
+export const useWarmUpBrowser = () => {
+  React.useEffect(() => {
+    if (Platform.OS !== "web") {
+      void WebBrowser.warmUpAsync();
+      return () => {
+        void WebBrowser.coolDownAsync();
+      };
+    }
+  }, []);
+};
+WebBrowser.maybeCompleteAuthSession();
+
+const Login = () => {
+  useWarmUpBrowser();
+  const [isLoading, setIsLoading] = React.useState(false);
+
+  return (
+    <SafeAreaView className="bg-white min-h-full">
+      <ScrollView>
+        <Stack.Screen options={{ headerShown: false }} />
+        <Text className="text-[28px] font-bold mt-48 ml-5">Welcome Back!</Text>
+        <Text className="text-[15px] ml-6 mt-1">Sign in to your account</Text>
+
+        {/* <LoginForm /> */}
+
+        <View className="flex-row items-center justify-center mt-6 mb-8 mx-6">
+          <View className="flex-1 h-[1px] bg-gray-300" />
+          <Text className="text-center mx-2 text-gray-600"> or </Text>
+          <View className="flex-1 h-[1px] bg-gray-300" />
+        </View>
+
+        {/* <GoogleSignInButton isLoading={isLoading} setIsLoading={setIsLoading} /> */}
+
+        <View className="flex-row items-center justify-center mt-6 mb-10">
+          <Text> Don't have an account? </Text>
+          <Link
+            href="/(auth)/register"
+            className="text-[#00657A] underline font-medium ml-1"
+          >
+            Create Account
+          </Link>
+        </View>
+      </ScrollView>
+    </SafeAreaView>
+  );
+};
+
+export default Login;
---
+++ src/app/(tabs)/home.tsx
@@ -0,0 +1,14 @@
+import { View, Text, Image } from "react-native";
+import React from "react";
+
+const Home = () => {
+  return (
+    <View className="p-4 bg-white min-h-full">
+      <View className="flex flex-row items-center mt-8">
+        <Text className="text-[20px] font-semibold">Welcome</Text>
+      </View>
+    </View>
+  );
+};
+
+export default Home;

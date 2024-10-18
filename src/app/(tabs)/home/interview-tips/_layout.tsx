973b139dbb900dbeb6379b1a7e9fd0f5a7881e13
Steven Ballaret <135417330+SteveBllrt@users.noreply.github.com>
2024-10-18 15:11:50 +0800

    feat(interview-tips): added interview tips  layout

---
+++ src/app/(tabs)/home/interview-tips/_layout.tsx
@@ -0,0 +1,37 @@
+import React from "react";
+import { router, Stack } from "expo-router";
+import { TouchableOpacity, Text } from "react-native";
+import { Ionicons } from "@expo/vector-icons";
+
+const InterviewTipsLayout = () => {
+  return (
+    <Stack
+      screenOptions={{
+        headerTitleAlign: "center",
+        headerShadowVisible: false,
+        headerLeft: () => (
+          <TouchableOpacity
+            style={{ padding: 1 }}
+            onPress={() => router.back()}
+          >
+            <Ionicons name="arrow-back" size={22} color="#2a2a2a" />
+          </TouchableOpacity>
+        ),
+      }}
+    >
+      <Stack.Screen
+        name="[id]"
+        options={{
+          headerTitle: () => (
+            <Text className="text-center text-[17px] font-bold text-[#2a2a2a]">
+              Interview Tips
+            </Text>
+          ),
+          headerBackVisible: false,
+        }}
+      />
+    </Stack>
+  );
+};
+
+export default InterviewTipsLayout;

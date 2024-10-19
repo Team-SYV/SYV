9e76e644eb9fb25b39f67d032fdb73c8b8b0e623
Steven Ballaret <135417330+SteveBllrt@users.noreply.github.com>
2024-10-19 14:31:54 +0800

    refactor(home): improve homepage ui

--- src/app/(tabs)/home/index.tsx
+++ src/app/(tabs)/home/index.tsx
@@ -29,15 +29,15 @@ const Home = () => {
         <Card
           imageSource={require("@/assets/images/virtual-interview.png")}
           text="Talk with Virtual Interviewer"
-          textClassName="text-[14px]"
-          onPress={() => router.push("/(tabs)/home/virtual-interview")}
+          textClassName="text-[13px]"
+          onPress={() => router.push("/(tabs)/home/virtual-interview/job-information")}
         />
       </View>
 
       <Card
         imageSource={require("@/assets/images/record-yourself.png")}
         text="Record Yourself"
-        textClassName="text-[15px]"
+        textClassName="text-[14px]"
         onPress={() => router.push("/(tabs)/home/record-yourself")}
       />
 

161c80434399f1bb7fbe6c58f661c4a6e56b9cd1
Steven Ballaret <135417330+SteveBllrt@users.noreply.github.com>
2024-10-19 14:23:44 +0800

    refactor(job-information): added step content type

---
+++ src/types/stepContent.ts
@@ -0,0 +1,12 @@
+import { JobInfo } from "./jobInfo";
+
+export interface StepContentProps {
+  activeStep: number;
+  formData: JobInfo;
+  updateFormData: (key: string, value: string, callback?: () => void) => void;
+  handleNextStep: () => void;
+  handleSubmit: () => void;
+  handleSubmitRoute: string;
+  handleSkip: () => void;
+  jobInformationId: string | null;
+}

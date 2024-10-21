60933c4788fb50fce57dbfce59477ddcd0158b49
Steven Ballaret <135417330+SteveBllrt@users.noreply.github.com>
2024-10-21 15:14:01 +0800

    feat(onboarding): added onboarding data

---
+++ src/constants/onboardingData.ts
@@ -0,0 +1,26 @@
+export const onBoarding = [
+  {
+    source: require("@/assets/images/onboarding1.png"),
+    title: "Welcome to Savy!",
+    description:
+      "Enhance your interview performance and confidence effortlessly, anytime and anywhere.",
+  },
+  {
+    source: require("@/assets/images/onboarding2.png"),
+    title: "Personalized Interview Questions",
+    description:
+      "Receive tailored interview questions based on your job information or uploaded resume.",
+  },
+  {
+    source: require("@/assets/images/onboarding3.png"),
+    title: "Practice with AI or Record Yourself",
+    description:
+      "Choose to practice with a virtual avatar or record yourself answering questions.",
+  },
+  {
+    source: require("@/assets/images/onboarding4.png"),
+    title: "Instant AI Feedback and Ratings",
+    description:
+      "Get instant ai feedback and ratings after each session to improve your performance.",
+  },
+];

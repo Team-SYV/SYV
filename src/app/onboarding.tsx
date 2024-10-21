33aeed4622db2dc0b0ed794fec535eacf05ce494
Steven Ballaret <135417330+SteveBllrt@users.noreply.github.com>
2024-10-21 15:13:34 +0800

    feat(onboarding): added onboarding ui

---
+++ src/app/onboarding.tsx
@@ -0,0 +1,107 @@
+import { useRouter } from "expo-router";
+import CustomButton from "@/components/Button/CustomButton";
+import React, { useEffect, useRef, useState } from "react";
+import {
+  View,
+  Image,
+  FlatList,
+  Dimensions,
+  Animated,
+  Text,
+} from "react-native";
+import { onBoarding } from "@/constants/onboardingData";
+
+const { width } = Dimensions.get("window");
+
+const OnboardingPage = () => {
+  const router = useRouter();
+  const [currentIndex, setCurrentIndex] = useState(0);
+  const flatListRef = useRef(null);
+  const scrollX = useRef(new Animated.Value(0)).current;
+
+  // Auto scroll every 3 seconds
+  useEffect(() => {
+    const interval = setInterval(() => {
+      const nextIndex = (currentIndex + 1) % onBoarding.length;
+      setCurrentIndex(nextIndex);
+      flatListRef.current.scrollToIndex({ index: nextIndex, animated: true });
+    }, 3000);
+    return () => clearInterval(interval);
+  }, [currentIndex]);
+
+  const handleScroll = (event) => {
+    const contentOffsetX = event.nativeEvent.contentOffset.x;
+    const newIndex = Math.floor(contentOffsetX / width);
+    setCurrentIndex(newIndex);
+    scrollX.setValue(contentOffsetX);
+  };
+
+  return (
+    <View className="flex-1">
+      <FlatList
+        ref={flatListRef}
+        data={onBoarding}
+        keyExtractor={(item, index) => index.toString()}
+        renderItem={({ item }) => (
+          <View style={{ width: width, alignItems: "center" }}>
+            <Image
+              source={item.source}
+              style={{
+                width: width,
+                height: 310,
+                resizeMode: "contain",
+                alignSelf: "center",
+              }}
+            />
+            <Text className="mt-5 text-[21px] font-semibold text-gray-800 px-6 text-center">
+              {item.title}
+            </Text>
+            <Text className="mt-1 mb-36 text-center text-gray-600 px-6 text-base">
+              {item.description}
+            </Text>
+          </View>
+        )}
+        horizontal
+        showsHorizontalScrollIndicator={false}
+        onScroll={handleScroll}
+        pagingEnabled
+        scrollEventThrottle={16}
+        contentContainerStyle={{ alignItems: "center", paddingVertical: 20 }}
+      />
+
+      {/* Page Indicator */}
+      <View className="absolute bottom-[210px] w-full flex-row justify-center z-10">
+        {onBoarding.map((_, index) => (
+          <View
+            key={index}
+            style={{
+              width: 9,
+              height: 9,
+              borderRadius: 4,
+              backgroundColor: currentIndex === index ? "#00AACE" : "#DFDFDF",
+              marginHorizontal: 5,
+            }}
+          />
+        ))}
+      </View>
+
+      <View className="absolute bottom-0 w-full px-6 py-8">
+        <CustomButton
+          title="Sign In"
+          onPress={() => router.push("/(auth)/login")}
+          containerStyles="bg-[#00AACE] h-[50px] w-full rounded-2xl"
+          textStyles="text-white text-[16px] font-medium"
+        />
+
+        <CustomButton
+          title="Sign Up"
+          onPress={() => router.push("/(auth)/register")}
+          containerStyles="border border-[#B5B5B5] h-[50px] w-full rounded-2xl mt-4"
+          textStyles="text-gray-600 text-[16px] font-medium"
+        />
+      </View>
+    </View>
+  );
+};
+
+export default OnboardingPage;

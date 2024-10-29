4b877a496b8afff6a822a8b96dad1b23527fb741
Yoshifumi Suzuki <yoshifumisuzuki81194@gmail.com>
2024-10-29 14:12:53 +0800

    chore(api.ts): changed file names of types to camel case

--- src/api.ts
+++ src/api.ts
@@ -2,8 +2,6 @@ import axios from "axios";
 import { AnswerData } from "./types/AnswerData";
 import { InterviewData } from "./types/InterviewData";
 import { JobInformationData } from "./types/JobInformationData";
-import { QuestionData } from "./types/QuestionData";
-import { FeedbackData } from "./types/FeedbackData";
 
 const api = axios.create({
   baseURL: process.env.EXPO_PUBLIC_BASE_URL,

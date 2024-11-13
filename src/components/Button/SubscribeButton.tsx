44ec4fe7945082a067ce34607feafbe2b7c89b21
Steven Ballaret <135417330+SteveBllrt@users.noreply.github.com>
2024-11-13 18:46:12 +0800

    feat(subscription): added subscription ui

--- src/components/Button/SubscribeButton.tsx
+++ src/components/Button/SubscribeButton.tsx
@@ -2,16 +2,23 @@ import React from "react";
 import { Pressable, Text, View } from "react-native";
 import MaterialIcons from "@expo/vector-icons/MaterialIcons";
 import GetPro from "./GetPro";
+import { Link } from "expo-router";
 
 const SubscribeButton = () => {
   return (
-    <View>
-      <Pressable className="flex-row items-center justify-between px-2 py-2 border border-gray-200 rounded-lg mx-6 mt-5">
+    <View className="mx-6 mt-5">
+      <Pressable className="flex-row items-center justify-between px-2 py-2 border border-gray-200 rounded-lg">
         <View className="flex-row items-center">
-          <MaterialIcons name="subscriptions" size={17} className="mr-2" />
+          <MaterialIcons
+            name="subscriptions"
+            size={17}
+            style={{ marginRight: 8 }}
+          />
           <Text className="text-[13px]">Subscribe to pro plan</Text>
         </View>
-        <GetPro />
+        <Link href="/profile/subscription">
+          <GetPro />
+        </Link>
       </Pressable>
     </View>
   );

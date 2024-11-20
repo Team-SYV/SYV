063196d64e3e50ce3d72a4e5c9b77885a931acc3
Steven Ballaret <135417330+SteveBllrt@users.noreply.github.com>
2024-11-20 19:20:17 +0800

    feat: added loading state ui

--- src/components/Modal/NextModal.tsx
+++ src/components/Modal/NextModal.tsx
@@ -1,6 +1,7 @@
 import React from "react";
-import { Modal, View, Text, ActivityIndicator } from "react-native";
+import { Modal, View, Text } from "react-native";
 import CustomButton from "../Button/CustomButton";
+import { LoadingDots } from "@mrakesh0608/react-native-loading-dots";
 
 interface NextModalProps {
   isVisible: boolean;
@@ -25,23 +26,41 @@ const NextModal: React.FC<NextModalProps> = ({
       onRequestClose={onClose}
     >
       <View className="flex-1 justify-center items-center bg-black/70">
-        <View className="bg-white rounded-xl items-center shadow-lg px-16 py-6">
-          <Text className="text-lg mb-6">
-            {isLastQuestion
-              ? "Get your interview results"
-              : "Ready for the next question?"}
-          </Text>
-          {isLoading ? (
-            <ActivityIndicator size="large" color="#00AACE" />
-          ) : (
+        {isLoading ? (
+          <View className="bg-white rounded-xl items-center shadow-lg py-4 px-5">
+            <Text className="text-[14px] font-medium mt-4 text-center">
+              We’re analyzing your video to generate feedback.
+            </Text>
+            <Text className="text-[14px] mt-3">
+              Please hold on for a moment.
+            </Text>
+            <LoadingDots
+              animation="pulse"
+              color="#00AACE"
+              containerStyle={{
+                padding: 12,
+              }}
+              dots={4}
+              size={12}
+              spacing={4}
+            />
+          </View>
+        ) : (
+          <View className="bg-white rounded-xl items-center shadow-lg px-16 py-6">
+            <Text className="text-lg mb-6">
+              {isLastQuestion
+                ? "Get your interview results"
+                : "Ready for the next question?"}
+            </Text>
+
             <CustomButton
               title={isLastQuestion ? "Get Results" : "Next"}
               onPress={onNext}
               containerStyles="bg-[#00AACE] h-[45px] rounded-3xl px-[82px]"
               textStyles="text-white text-base font-semibold"
             />
-          )}
-        </View>
+          </View>
+        )}
       </View>
     </Modal>
   );

e52f1a76c370b3da6e9363cfd668f98ef5df5cc7
Steven Ballaret <135417330+SteveBllrt@users.noreply.github.com>
2024-12-03 01:55:43 +0800

    feat(ratings): added loading state before accesing the data

--- src/app/(tabs)/home/virtual-interview/ratings.tsx
+++ src/app/(tabs)/home/virtual-interview/ratings.tsx
@@ -8,7 +8,7 @@ import { getRatings } from "@/api";
 const Ratings = () => {
   const [loading, setLoading] = useState(false);
   const router = useRouter();
-  const {interviewId} = useLocalSearchParams();
+  const { interviewId } = useLocalSearchParams();
   const [fetchedRatings, setFetchedRatings] = useState({
     relevance: 0,
     grammar: 0,
@@ -24,22 +24,25 @@ const Ratings = () => {
     pace: fetchedRatings.pace,
     fillerWords: fetchedRatings.fillerWords,
   };
-  
-useEffect(() => {
+
+  // Fetch the ratings
+  useEffect(() => {
     const fetch = async () => {
       try {
+        setLoading(true);
         const fetchedRatings = await getRatings(interviewId);
-        console.log(fetchedRatings)
+        console.log(fetchedRatings);
         setFetchedRatings({
           relevance: fetchedRatings[0].answer_relevance,
           grammar: fetchedRatings[0].grammar,
           eyeContact: fetchedRatings[0].eye_contact,
           pace: fetchedRatings[0].pace_of_speech,
           fillerWords: fetchedRatings[0].filler_words,
-      
         });
       } catch (error) {
         console.error("Error fetching data", error.message);
+      } finally {
+        setLoading(false);
       }
     };
     if (interviewId) {
@@ -54,7 +57,6 @@ useEffect(() => {
   const numberOfStars = 5;
   const filledStars = Math.round(overallRating);
 
-  
   const renderProgressBar = (progress) => (
     <View className="w-full h-4 bg-gray-200 rounded-full">
       <View
@@ -75,67 +77,72 @@ useEffect(() => {
   return (
     <View className="flex-1 p-6 bg-white">
       <Spinner visible={loading} color="#00AACE" />
-      <Text className="text-[18px] text-center font-medium">
-        Overall Rating
-      </Text>
+      
+      {!loading && (
+        <>
+          <Text className="text-[18px] text-center font-medium">
+            Overall Rating
+          </Text>
 
-      <Text className="text-[70px] text-center font-medium">
-        {overallRating.toFixed(0)}/5
-      </Text>
+          <Text className="text-[70px] text-center font-medium">
+            {overallRating.toFixed(0)}/5
+          </Text>
 
-      <View className="flex-row items-center justify-center">
-        {[...Array(numberOfStars)].map((_, index) => (
-          <View key={index} className="mx-2">
-            <AntDesign
-              name={index < filledStars ? "star" : "staro"}
-              size={30}
-              color={index < filledStars ? "#FFC300" : "gray"}
-            />
+          <View className="flex-row items-center justify-center">
+            {[...Array(numberOfStars)].map((_, index) => (
+              <View key={index} className="mx-2">
+                <AntDesign
+                  name={index < filledStars ? "star" : "staro"}
+                  size={30}
+                  color={index < filledStars ? "#FFC300" : "gray"}
+                />
+              </View>
+            ))}
           </View>
-        ))}
-      </View>
 
-      <View className="mt-14">
-        <View className="flex-row justify-between items-center mb-2">
-          <Text className="text-[14px] font-medium">Answer Relevance</Text>
-          <Text className="text-[14px] font-medium mr-2">
-            {progressData.relevance}
-          </Text>
-        </View>
-        {renderProgressBar(progressData.relevance)}
+          <View className="mt-14">
+            <View className="flex-row justify-between items-center mb-2">
+              <Text className="text-[14px] font-medium">Answer Relevance</Text>
+              <Text className="text-[14px] font-medium mr-2">
+                {progressData.relevance}
+              </Text>
+            </View>
+            {renderProgressBar(progressData.relevance)}
 
-        <View className="flex-row justify-between items-center mb-2 mt-9">
-          <Text className="text-[14px] font-medium">Grammar</Text>
-          <Text className="text-[14px] font-medium mr-2">
-            {progressData.grammar}
-          </Text>
-        </View>
-        {renderProgressBar(progressData.grammar)}
+            <View className="flex-row justify-between items-center mb-2 mt-9">
+              <Text className="text-[14px] font-medium">Grammar</Text>
+              <Text className="text-[14px] font-medium mr-2">
+                {progressData.grammar}
+              </Text>
+            </View>
+            {renderProgressBar(progressData.grammar)}
 
-        <View className="flex-row justify-between items-center mb-2 mt-9">
-          <Text className="text-[14px] font-medium">Eye Contact</Text>
-          <Text className="text-[14px] font-medium mr-2">
-            {progressData.eyeContact}
-          </Text>
-        </View>
-        {renderProgressBar(progressData.eyeContact)}
+            <View className="flex-row justify-between items-center mb-2 mt-9">
+              <Text className="text-[14px] font-medium">Eye Contact</Text>
+              <Text className="text-[14px] font-medium mr-2">
+                {progressData.eyeContact}
+              </Text>
+            </View>
+            {renderProgressBar(progressData.eyeContact)}
 
-        <View className="flex-row justify-between items-center mb-2 mt-9">
-          <Text className="text-[14px] font-medium">Pace of Speech</Text>
-          <Text className="text-[14px] font-medium mr-2">
-            {progressData.pace}
-          </Text>
-        </View>
-        {renderProgressBar(progressData.pace)}
+            <View className="flex-row justify-between items-center mb-2 mt-9">
+              <Text className="text-[14px] font-medium">Pace of Speech</Text>
+              <Text className="text-[14px] font-medium mr-2">
+                {progressData.pace}
+              </Text>
+            </View>
+            {renderProgressBar(progressData.pace)}
 
-        <View className="flex-row justify-between items-center mb-2 mt-9">
-          <Text className="text-[14px] font-semibold">Filler Words</Text>
-          <Text className="text-[14px] font-medium mr-2">
-            {progressData.fillerWords}
-          </Text>
-        </View>
-        {renderProgressBar(progressData.fillerWords)}
-      </View>
+            <View className="flex-row justify-between items-center mb-2 mt-9">
+              <Text className="text-[14px] font-semibold">Filler Words</Text>
+              <Text className="text-[14px] font-medium mr-2">
+                {progressData.fillerWords}
+              </Text>
+            </View>
+            {renderProgressBar(progressData.fillerWords)}
+          </View>
+        </>
+      )}
 
       <TouchableOpacity
         onPress={handleGoBack}

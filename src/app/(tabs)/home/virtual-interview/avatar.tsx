738851f0fb724291729c98b70dfc940f4b128acd
Steven Ballaret <135417330+SteveBllrt@users.noreply.github.com>
2024-12-09 18:04:48 +0800

    feat(avatar): change model to full body

--- src/app/(tabs)/home/virtual-interview/avatar.tsx
+++ src/app/(tabs)/home/virtual-interview/avatar.tsx
@@ -2,19 +2,25 @@ import React, { Suspense } from "react";
 import { Canvas } from "@react-three/fiber";
 import { Model } from "@/components/Avatar/Model";
 import useControls from "r3f-native-orbitcontrols";
+import { Loader } from "@react-three/drei";
 
 export default function Avatar() {
   const [OrbitControls, events] = useControls();
 
   return (
-    <Canvas>
-      <OrbitControls />
-      <Suspense fallback={null}>
-        <ambientLight />
-        <directionalLight position={[5, 5, 5]} />
-        <Model />
-      </Suspense>
-      <perspectiveCamera position={[0, 1, 3]} fov={60} />
-    </Canvas>
+    <>
+    <Loader/>
+      <Canvas
+        style={{ width: "100%", height: "50" }}
+        camera={{ position: [0, 1.5, 5], fov: 45 }}
+      >
+        <OrbitControls />
+        <Suspense fallback={null}>
+          <ambientLight intensity={0.8} />
+          <directionalLight position={[5, 5, 5]} />
+          <Model />
+        </Suspense>
+      </Canvas>
+    </>
   );
 }

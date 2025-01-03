d165d6da2a533e62b9835ee12b8e081a69da7d53
Steven Ballaret <135417330+SteveBllrt@users.noreply.github.com>
2025-01-03 16:51:23 +0800

    refactor(login): adjust margins in ui

--- src/app/(auth)/login.tsx
+++ src/app/(auth)/login.tsx
@@ -8,7 +8,7 @@ const Login = () => {
     <SafeAreaView className="bg-white min-h-full">
       <ScrollView>
         <Stack.Screen options={{ headerShown: false }} />
-        <Text className="text-[26px] font-semibold mt-52 ml-5">
+        <Text className="text-[26px] font-semibold mt-56 ml-5">
           Welcome Back!
         </Text>
         <Text className="text-[15px] ml-6 mt-1">Sign in to your account</Text>

341d16fc666986d0f63f892ae2fb34d45d845d32
Steven Ballaret <135417330+SteveBllrt@users.noreply.github.com>
2025-01-09 07:39:42 +0800

    refactr: fix the error validation

--- src/utils/validators/validateJobInfo.ts
+++ src/utils/validators/validateJobInfo.ts
@@ -1,12 +1,18 @@
 export const validateStep = (step: number, formData: any) => {
   switch (step) {
     case 0:
-      return formData.selectedIndustry !== null;
-    case 1:
-      return formData.selectedJobRole !== null;
+      return formData.selectedJobDescription !== null;
     case 2:
-      return formData.selectedInterviewType !== null;
+      return formData.selectedResume !== null;
+    case 2:
+      return formData.selectedIndustry !== null;
     case 3:
+      return formData.selectedJobRole !== null;
+    case 4:
+      return formData.selectedCompany !== null;
+    case 5:
+      return formData.selectedInterviewType !== null;
+    case 6:
       return formData.selectedExperienceLevel !== null;
     default:
       return true;
@@ -16,12 +22,18 @@ export const validateStep = (step: number, formData: any) => {
 export const getErrorMessage = (step: number) => {
   switch (step) {
     case 0:
-      return "Please select an industry.";
+      return "Please upload a job description.";
     case 1:
-      return "Please select a job role.";
+      return "Please upload a resume.";
     case 2:
-      return "Please select an interview type.";
+      return "Please select an industry.";
     case 3:
+      return "Please select a job role.";
+    case 4:
+      return "Please input a company name.";
+    case 5:
+      return "Please select an interview type.";
+    case 6:
       return "Please select an experience level.";
     default:
       return "";

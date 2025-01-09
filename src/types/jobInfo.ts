ed98b57df36ee65e13caa6e22e6dbed180ac8fcc
Steven Ballaret <135417330+SteveBllrt@users.noreply.github.com>
2025-01-09 10:25:01 +0800

    refactor: fixed the type of job info

--- src/types/jobInfo.ts
+++ src/types/jobInfo.ts
@@ -1,8 +1,15 @@
+export interface FileObject {
+  uri: string; 
+  name: string; 
+  type: string; 
+}
+
 export interface JobInfo {
+  selectedJobDescription: FileObject | null;
+  selectedResume: FileObject | null;
   selectedIndustry: string | null;
   selectedJobRole: string | null;
+  selectedCompany: string | null;
   selectedInterviewType: string | null;
   selectedExperienceLevel: string | null;
-  companyName: string;
-  jobDescription: string;
 }
